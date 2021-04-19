2701ae4b908b09f154dac1ea30a7b2edbade9f94
Rossi Kwak <rossi.kwak@gmail.com>
2021-04-19 14:15:39 +0900

    porting on the way from https://github.com/aim2nasa/ndk commit:178355c85ebdd7ecdcbe53618b548b63c5d80b0a

--- gStreamer/app/src/main/java/com/example/gstreamer/MainActivity.java
+++ gStreamer/app/src/main/java/com/example/gstreamer/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
+        TextView tv = findViewById(R.id.tvHello);
         tv.setText(stringFromJNI());
     }
 
