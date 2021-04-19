7e919d66f93fd8056bda95bfa7d0395292d72db3
Rossi Kwak <rossi.kwak@gmail.com>
2021-04-19 14:33:10 +0900

    porting on the way from https://github.com/aim2nasa/ndk commit:178355c85ebdd7ecdcbe53618b548b63c5d80b0a, read JNI string from the native library

--- gStreamer/app/src/main/java/com/example/gstreamer/MainActivity.java
+++ gStreamer/app/src/main/java/com/example/gstreamer/MainActivity.java
@@ -102,6 +102,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ((TextView)findViewById(R.id.tvHello)).setText(stringFromJNI());
+
         createButtons();
         createList();
 
