d8222aacfa69955b06d0ef315f4d1e651c43b3ed
Rossi Kwak <rossi.kwak@gmail.com>
2021-09-27 13:57:49 +0900

    version 0.1.5

--- gStreamer/app/src/main/cpp/libglsusb.cpp
+++ gStreamer/app/src/main/cpp/libglsusb.cpp
@@ -574,7 +574,7 @@ Java_com_example_gstreamer_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
 
-    std::string hello = "gStreamer Android v0.1.4";
+    std::string hello = "gStreamer Android v0.1.5";
     return env->NewStringUTF(hello.c_str());
 }
 
