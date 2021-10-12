ebb34da5f8f3bf261560d97e6ab4ef556ceac8c6
Rossi Kwak <rossi.kwak@gmail.com>
2021-10-12 10:26:17 +0900

    add empty activity 'BpsView'

---
+++ gStreamer/app/src/main/java/com/example/gstreamer/BpsView.java
@@ -0,0 +1,14 @@
+package com.example.gstreamer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BpsView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bps_view);
+    }
+}
