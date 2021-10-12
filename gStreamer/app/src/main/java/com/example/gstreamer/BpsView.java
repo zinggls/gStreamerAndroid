b3f7388c41a24805bf00088f48ab5fd7b2bb1ca8
Rossi Kwak <rossi.kwak@gmail.com>
2021-10-12 14:22:40 +0900

    create button in bpsView and back to main window on click button

--- gStreamer/app/src/main/java/com/example/gstreamer/BpsView.java
+++ gStreamer/app/src/main/java/com/example/gstreamer/BpsView.java
@@ -3,12 +3,24 @@ package com.example.gstreamer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class BpsView extends AppCompatActivity {
+    private Button btnMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bps_view);
+
+        btnMain = (Button) findViewById(R.id.button_main);
+
+        btnMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
